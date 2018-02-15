87f96b8ad921dea753bca1db11d3eae52f35cc5d
Alix Rive-Decaillot <rive.decaillot.a@gmail.com>
2018-02-15 17:47:27 +0100

    Correction tests

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ta gueule', anyArray: [1,2,3] });
+  res.render('index', { title: 'Form Validation', success: false, errors: req.session.errors });
+  req.session.errors = null;
 });
 
-router.get('/test/:id/:param2', function(res, req, next) {
-  res.render('test', {output: req.params.param2});
+router.post('/submit', function(req, res, next) {
+  req.check('email', 'Invalid email adress').isEmail();
+  req.check('password', 'Password is invalid').isLength({min: 8}).equals(req.body.confirmPassword);
+
+  var errors = req.validationErrors();
+  if (errors) {
+    req.session.errors = errors;
+  }
+  res.redirect('/');
 })
 
-router.post('/test/submit', function(req, res, next){
-  var id = req.body.id;
-  res.redirect('/test/' + id);
-})
+// router.get('/test/:id/:param2', function(res, req, next) {
+//   res.render('test', {output: req.params.param2});
+// })
+
+// router.post('/test/submit', function(req, res, next){
+//   var id = req.body.id;
+//   res.redirect('/test/' + id);
+// })
 
 module.exports = router;
