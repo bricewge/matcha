437b2df94999f2aa2cf16cc769dc5cfa27c3421e
Brice Waegeneire <brice.wge@gmail.com>
2018-02-21 16:16:48 +0100

    password validation done

--- server/src/helpers/validate.js
+++ server/src/helpers/validate.js
@@ -1,26 +1,44 @@
-const helperObj = require('./object.js')
+const {readFileSync} = require('fs')
+const path = require('path')
+const {keyIsUnique} = require('./object.js')
 
 exports.email = function (email) {
   let regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return regexp.test(String(email).toLowerCase())
 }
 
-// TODO Test against 100000 common passowrds
-// TODO Password should be unique in a user's data (ie: email, username, ...)
 exports.password = function (input) {
-  for (let key in input) { input[key].toLowerCase() }
-  // console.log(input)
+  for (let key in input) { input[key] = input[key].toLowerCase() }
   if (input.password.length < 8) return false
-  if (!helperObj.keyIsUnique(input, 'password')) return false
+  if (!keyIsUnique(input, 'password')) return false
+  if (passwordIsCommon(input.password)) return false
   return true
 }
 
+// TODO Make it async
+function passwordIsCommon (password) {
+  let passwordsPath = path.join(__dirname, '../misc/passwords.txt')
+  let commonPasswords = readFileSync(passwordsPath, 'utf8').trim().split('\n')
+  password = password.toLowerCase()
+  for (let key in commonPasswords) {
+    if (password === commonPasswords[key]) return true
+  }
+  return false
+}
+
 // Should return false
-var data = {
-  userName: 'qwertyui',
+var tes01 = {
+  userName: 'toto',
   email: 'to@t.o',
-  password: 'qwertyui',
+  password: 'QWERTYUI',
   firstName: 'Toto',
   lastName: 'OTOT'}
-
-console.log(exports.password(data))
+// Should return false
+var test02 = {
+  userName: 'toto',
+  email: 'to@t.o',
+  password: 'qwertyuisalut',
+  firstName: 'Toto',
+  lastName: 'OTOT'}
+// console.log(exports.password(tes01))
+// console.log(exports.password(test02))
