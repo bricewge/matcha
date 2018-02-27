aeb4c7dd9d7682b533c6fc20c252abe60da8f617
Brice Waegeneire <brice.wge@gmail.com>
2018-02-27 21:54:00 +0100

    fix user popover

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -18,45 +18,60 @@
          v-if="!$store.state.isUserLoggedIn"
          >Sign In</v-btn>
   <v-menu
+    v-if="$store.state.isUserLoggedIn"
     :close-on-content-click="false"
-    bottom
     v-model="menu"
+    offset-y
+    bottom
     class="mr-3"
     >
     <v-avatar color="primary"
               :size=36
-              v-if="$store.state.isUserLoggedIn"
               slot="activator">
       <span class="white--text headline">J</span>
     </v-avatar>
     <v-card>
-        <v-list>
+        <v-list >
           <v-list-tile avatar>
             <v-list-tile-avatar>
               <img src="" alt="Profile">
             </v-list-tile-avatar>
             <v-list-tile-content>
-              <v-list-tile-title>Firstname Lastname</v-list-tile-title>
+              <v-list-tile-title>Username</v-list-tile-title>
               <v-list-tile-sub-title>Email</v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list>
         <v-divider></v-divider>
-      <v-btn flat class="btn--plain"
-             router to="login"
-             v-if="$store.state.isUserLoggedIn"
-             >Log out</v-btn>
+    <v-list>
+      <v-list-tile v-for="item in items" :key="item.title" @click="">
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
     </v-card>
   </v-menu>
 </v-toolbar>
 </template>
 
 <!-- TODO Logout should logout and redirect to login -->
-<!-- TODO Logout should be under a popup on the login account
-     https://vuetifyjs.com/en/components/menus#example-popover -->
 <!-- TODO If the user has a profile picture display it -->
 <script>
 export default {
+    data () {
+        return {
+            menu: null,
+            items: [
+                { title: 'My profile', icon: 'account_box', action: 'profile' },
+                { title: 'Settings', icon: 'settings', action: 'settings' },
+                { title: 'Log out', icon: 'lock', action: 'logout' }
+            ]
+        }
+    }
 }
 </script>
 
