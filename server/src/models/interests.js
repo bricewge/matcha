266cc897b42511d38ea63805baac2905fc674808
Brice Waegeneire <brice.wge@gmail.com>
2018-02-28 18:16:52 +0100

    basic model for interest

--- server/src/models/interests.js
+++ server/src/models/interests.js
@@ -1,13 +1,38 @@
 const db = require('../db')
+const validate = require('../helpers/validate')
 
-exports.create = function (id, interest) {
-  const query = 'INSERT INTO interests (userId, interest) VALUES(?, ?);'
-  return db.get().queryAsync(query, [id, interest])
+exports.create = async function (input) {
+  const columns = ['userId', 'path']
+  validate.input(input, columns)
+  const query = 'INSERT INTO interests SET ?;'
+  let result = await db.get().queryAsync(query, input)
+  return exports.getAllById({id: result.insertId})
 }
 
-exports.delete = function (id, interest) {
-  const query = 'DELETE FROM interests WHERE userId = ? AND interest = ?;'
-  return db.get().queryAsync(query, [id, interest])
+exports.delete = async function (input) {
+  validate.input(input, ['id'])
+  const query = 'DELETE FROM interests WHERE id = ?;'
+  return db.get().queryAsync(query, input.id)
+}
+
+exports.getAllById = async function (input) {
+  validate.input(input, ['id'])
+  const query = 'SELECT * FROM interests WHERE id = ?;'
+  const result = await db.get().queryAsync(query, input.id)
+  return result[0]
+}
+
+exports.getAllByUserId = async function (input) {
+  validate.input(input, ['userId'])
+  const query = 'SELECT * FROM interests WHERE userId = ?;'
+  const result = await db.get().queryAsync(query, input.userId)
+  return result
+}
+
+exports.getUniqueInterest = async function () {
+  const query = 'SELECT DISTINCT interest FROM interests;'
+  const result = await db.get().queryAsync(query)
+  return result
 }
 
 exports.createTable = function () {
