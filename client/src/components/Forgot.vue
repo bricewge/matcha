b5912cb9f952af72ed00d45bc498c3eaf6f5cd8c
Brice Waegeneire <brice.wge@gmail.com>
2018-03-01 19:05:43 +0100

    create component for form filled by visitors, refactored Forgot.vue

--- client/src/components/Forgot.vue
+++ client/src/components/Forgot.vue
@@ -1,82 +1,64 @@
 <template>
-<v-container fluid fill-height class="grey lighten-1" >
-  <v-layout align-center justify-center>
-    <v-flex xs12 sm8 md4>
-      <v-card class="elevation-6">
-        <v-card-title class="grey lighten-4">
-          <v-toolbar-title class="text-xs-center">
-            Forgot your password?</v-toolbar-title>
-        </v-card-title>
-        <v-form
-          v-model="validForm"
-          ref="form"
-          @submit.prevent="forgot"
-          lazy-validation>
-          <v-card-text>
-            <v-text-field
-              name="email"
-              label="Email"
-              v-model="email"
-              :rules="emailRules"
-              ></v-text-field>
-            <v-alert
-              ref="alert"
-              :type="alertType"
-              v-model="alert"
-              dismissible
-              transition="scale-transition"
-              >{{ alertMessage }}</v-alert>
-          </v-card-text>
-          <v-card-actions class="mx-2 pb-3">
-            <router-link
-              to="/login"
-              class="ml-3 password grey--text text--darken-1"
-              >Login</router-link>
-            <v-spacer></v-spacer>
-            <v-btn
-              type="submit"
-              color="primary"
-              >Send me instructions</v-btn>
-          </v-card-actions>
-        </v-form>
-      </v-card>
-    </v-flex>
+<visitor-form
+  title="Forgot your password?"
+  :alert="alert"
+  :form="form"
+  ref="visitorForm">
+  <v-layout slot="fields">
+    <v-text-field
+      name="email"
+      label="Email"
+      v-model="email"
+      :rules="emailRules"
+      ></v-text-field>
   </v-layout>
-</v-container>
+  <v-layout slot="actions">
+    <router-link
+      to="/login"
+      class="ml-3 password grey--text text--darken-1"
+      >Login</router-link>
+    <v-spacer></v-spacer>
+    <v-btn
+      type="submit"
+      color="primary"
+      >Send me instructions</v-btn>
+  </v-layout>
+</visitor-form>
 </template>
 
 <script>
+import VisitorForm from '@/components/VisitorForm'
 import AuthenticationService from '@/services/AuthenticationService'
 import {validEmail} from '@/util/validation'
 
 export default {
+  components: {
+    VisitorForm
+  },
+
   data () {
     return {
-      validForm: true,
       email: '',
       emailRules: [validEmail],
-      alert: false,
-      alertType: 'info',
-      alertMessage : ''
+      alert: { type: 'error', visible: false, message: '' },
+      form: { calid: 'false', submit: this.forgot }
     }
   },
 
   methods: {
     async forgot () {
-      console.log(this.$refs.alert)
-      if (!this.$refs.form.validate()) return
+      if (!this.$refs.visitorForm.$refs.form.validate()) return
       try {
         const response = await AuthenticationService.forgot({
           email: this.email
         })
-        this.alertType = 'success'
-        this.alert = true
-        this.alertMessage = response.data.message
+        this.alert.type = 'success'
+        this.alert.message = response.data.message
+        this.alert.visible = true
       } catch (err) {
-        console.log(err)
-        this.alertType = 'error',
-        this.alert = true
-        this.alertMessage = err.response.data.message
+        this.alert.type = 'error'
+        this.alert.message = err.response.data.message
+        this.alert.visible = true
       }
     }
   }
---
+++ client/src/components/VisitorForm.vue
@@ -0,0 +1,62 @@
+<template>
+<v-container fluid fill-height class="grey lighten-1" >
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <v-card class="elevation-6">
+        <v-card-title class="grey lighten-4">
+          <v-toolbar-title class="text-xs-center">
+            {{ title }}</v-toolbar-title>
+        </v-card-title>
+        <v-form
+          v-model="form.valid"
+          ref="form"
+          @submit.prevent="form.submit"
+          lazy-validation>
+          <v-card-text>
+           <slot name="fields">No fields defined</slot>
+            <v-alert
+              ref="alert"
+              :type="alert.type"
+              v-model="alert.visible"
+              dismissible
+              transition="scale-transition"
+              >{{ alert.message }}</v-alert>
+          </v-card-text>
+          <v-card-actions class="pb-3">
+           <slot name="actions">No actions defined</slot>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Title'
+    },
+    form: {
+      type: Object,
+      default: () => ({
+        valid: true,
+        submit: () => {}
+      })
+    },
+    alert: {
+      type: Object,
+      default: () => ({
+        type: 'error',
+        visible: false,
+        message: 'Error message'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
